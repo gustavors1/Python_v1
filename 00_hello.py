4acfb5d8a05d379b4d0df6e339b2fb5bd98c32f8
Gustavo A. Rivera <123112483+gustavors1@users.noreply.github.com>
2023-05-08 14:05:11 +0200

    Confirmación

---
+++ 00_hello.py
@@ -0,0 +1,19 @@
+# Esto es un comentario:
+# Hola mundo
+print("hola mundo")
+
+
+"""
+Este es un
+comentario 
+en varias 
+lineas
+""" 
+#Consultar el tipo de dato
+print(type("Hola mundo")) # Tipo "str"
+print(type(5)) # Tipo "int"
+print(type(5.1)) # Tipo "float" 
+print(type(True)) # Tipo "booleam"
+print(type(3 + 1j)) # Tipo "complejo"
+
+print(type(print("Mi cadena de texto"))) #Tipo "NoneType"
